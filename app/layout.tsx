68209d637854d7556bf54d969a1a482300bddf4e
Neha Rajesh <leehanseok3@gmail.com>
2025-04-15 14:33:01 +0530

    init: basic setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,3 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
@@ -14,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "TuNeh Expense Tracker",
+  description: "Our household expense tracker",
 };
 
 const geistSans = Geist({
@@ -42,32 +37,16 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+                    <Link href={"/"}>TuNeh Expense Tracker</Link>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <div>Total This Month: Rs. 0</div>
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16"></footer>
             </div>
           </main>
         </ThemeProvider>
---
+++ components/addExpense/index.tsx
@@ -0,0 +1,67 @@
+'use client";';
+
+import { Dialog } from "radix-ui";
+import { Cross2Icon } from "@radix-ui/react-icons";
+
+export const AddExpenseButton = () => {
+  const handleSave = () => {
+    console.log("Expense saved");
+  };
+  return (
+    <>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <button className="Button violet">Add Expense</button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="DialogOverlay" />
+          <Dialog.Content className="DialogContent">
+            <Dialog.Title className="DialogTitle">Add Expense</Dialog.Title>
+            <Dialog.Description className="DialogDescription">
+              Add your expense details here. Click the save button once done.
+            </Dialog.Description>
+
+            <div>
+              <fieldset className="Fieldset">
+                <label className="Label" htmlFor="name">
+                  Title
+                </label>
+                <input
+                  className="Input"
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                />
+              </fieldset>
+              <fieldset className="Fieldset">
+                <label className="Label" htmlFor="username">
+                  Type
+                </label>
+                <input
+                  className="Input"
+                  id="username"
+                  defaultValue="@peduarte"
+                />
+              </fieldset>
+              <fieldset className="Fieldset">
+                <label className="Label" htmlFor="amount">
+                  Amount
+                </label>
+                <input className="Input" id="amount" defaultValue="0" />
+              </fieldset>
+            </div>
+
+            <div>
+              <button onClick={handleSave}> Save Changes</button>
+            </div>
+
+            <Dialog.Close asChild>
+              <button className="IconButton" aria-label="Close">
+                <Cross2Icon />
+              </button>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </>
+  );
+};
