2b566667c5bda52f1179b059ad3d6c1a330e182b
Neha Rajesh <leehanseok3@gmail.com>
2025-05-09 10:29:17 +0530

    refactor: remove radix ui

--- components/addExpense/index.tsx
+++ components/addExpense/index.tsx
@@ -1,7 +1,5 @@
 'use client";';
 
-import { Dialog } from "radix-ui";
-import { Cross2Icon } from "@radix-ui/react-icons";
 import "./index.css";
 
 export const AddExpenseButton = () => {
@@ -10,60 +8,61 @@ export const AddExpenseButton = () => {
   };
 
   return (
-    <>
-      <Dialog.Root>
-        <Dialog.Trigger asChild>
-          <button className="triggerButton">+ Add Expense</button>
-        </Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay className="DialogOverlay" />
-          <Dialog.Content className="DialogContent">
-            <Dialog.Title className="DialogTitle">Add Expense</Dialog.Title>
-            <Dialog.Description className="DialogDescription">
-              Add your expense details here. Click the save button once done.
-            </Dialog.Description>
+    <></>
+    // <>
+    //   <Dialog.Root>
+    //     <Dialog.Trigger asChild>
+    //       <button className="triggerButton">+ Add Expense</button>
+    //     </Dialog.Trigger>
+    //     <Dialog.Portal>
+    //       <Dialog.Overlay className="DialogOverlay" />
+    //       <Dialog.Content className="DialogContent">
+    //         <Dialog.Title className="DialogTitle">Add Expense</Dialog.Title>
+    //         <Dialog.Description className="DialogDescription">
+    //           Add your expense details here. Click the save button once done.
+    //         </Dialog.Description>
 
-            <div>
-              <fieldset className="Fieldset">
-                <label className="Label" htmlFor="name">
-                  Title
-                </label>
-                <input
-                  className="Input"
-                  id="name"
-                  defaultValue="Pedro Duarte"
-                />
-              </fieldset>
-              <fieldset className="Fieldset">
-                <label className="Label" htmlFor="username">
-                  Type
-                </label>
-                <input
-                  className="Input"
-                  id="username"
-                  defaultValue="@peduarte"
-                />
-              </fieldset>
-              <fieldset className="Fieldset">
-                <label className="Label" htmlFor="amount">
-                  Amount
-                </label>
-                <input className="Input" id="amount" defaultValue="0" />
-              </fieldset>
-            </div>
+    //         <div>
+    //           <fieldset className="Fieldset">
+    //             <label className="Label" htmlFor="name">
+    //               Title
+    //             </label>
+    //             <input
+    //               className="Input"
+    //               id="name"
+    //               defaultValue="Pedro Duarte"
+    //             />
+    //           </fieldset>
+    //           <fieldset className="Fieldset">
+    //             <label className="Label" htmlFor="username">
+    //               Type
+    //             </label>
+    //             <input
+    //               className="Input"
+    //               id="username"
+    //               defaultValue="@peduarte"
+    //             />
+    //           </fieldset>
+    //           <fieldset className="Fieldset">
+    //             <label className="Label" htmlFor="amount">
+    //               Amount
+    //             </label>
+    //             <input className="Input" id="amount" defaultValue="0" />
+    //           </fieldset>
+    //         </div>
 
-            <div>
-              <button onClick={handleSave}> Save Changes</button>
-            </div>
+    //         <div>
+    //           <button onClick={handleSave}> Save Changes</button>
+    //         </div>
 
-            <Dialog.Close asChild>
-              <button className="IconButton" aria-label="Close">
-                <Cross2Icon />
-              </button>
-            </Dialog.Close>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-    </>
+    //         <Dialog.Close asChild>
+    //           <button className="IconButton" aria-label="Close">
+    //             <Cross2Icon />
+    //           </button>
+    //         </Dialog.Close>
+    //       </Dialog.Content>
+    //     </Dialog.Portal>
+    //   </Dialog.Root>
+    // </>
   );
 };
